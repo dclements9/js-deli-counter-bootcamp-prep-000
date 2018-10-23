e12b571b9786fb58cfefcc02b5a8d8125961b758
dclements <dclements@protonmail.com>
2018-10-23 15:02:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ function takeANumber(quantity,name){
 
   return `Welcome, ${name}. You are number ${position} in line.`
 }
+
+function nowServing(line){
+    if (line.length === 0){
+      return "There is nobody waiting to be served!"
+    }else{
+      return line(0)
+      line.pop()
+    }
+
+}
