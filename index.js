d252fc030ec39a38b9a2d26b74ff63f4ad23f6a8
dclements <dclements@protonmail.com>
2018-10-23 14:55:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function takeANumber(quantity,name){
     var line = []
     quantity.push(name)
-    var position = quantity.length - 1
+    var position = quantity.length
 
   return `Welcome, ${name}. You are number ${position} in line.`
 }
